f457c1072a5f58fc466b525b0b28517e1df123f4
Mohammed Arshad Hassan <hasan.arshad18@gmail.com>
2021-08-04 14:15:29 +0530

    Create initial scaffolding

---
+++ src/Joke.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Joke extends Component{
+    render(){
+        return(
+            <p>A Joke</p>
+        )
+    }
+};
+
+export default Joke;
---
+++ src/JokesContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Joke from './Joke';
+
+class JokeContainer extends Component{
+    render(){
+        <div>
+            <Joke />
+            <Joke />
+        </div>
+    }
+};
+export default JokeContainer;
